32415207589d3cd1b61ad91a657027d2b959ab8c
marcovins <marcosbelods@gmail.com>
2024-04-26 00:08:32 -0300

    Desafio de Código I

--- conta_bancaria.py
+++ conta_bancaria.py
@@ -1,32 +1,42 @@
+# Programa: Conta Bancária
+# Autor: [Marcos Belo]
+# Data: [26/04/2024]
+# Descrição: Este programa permite ao usuário realizar operações básicas de gerenciamento de finanças, como depósito, saque e visualização de extrato.
+
 import os
 
-saque_diario = 0
-saldo = 0
-extrato = ""
+# Inicialização de variáveis
+saque_diario = 0  # Contador de saques diários
+saldo = 0         # Saldo inicial
+extrato = ""      # Extrato inicial
 
+# Função para realizar um depósito
 def deposito(saldo, extrato, valor):
     saldo += valor
-    extrato += f"Deposito de R$ {valor:.2f} realizado.\n"
+    extrato += f"Deposito de R$ {valor:.2f} realizado.\n"  # Adiciona o depósito ao extrato
     return saldo, extrato
 
+# Função para realizar um saque
 def saque(saldo, extrato, valor):
     saldo -= valor
-    extrato += f"Saque de R$ {valor:.2f} realizado.\n"
+    extrato += f"Saque de R$ {valor:.2f} realizado.\n"     # Adiciona o saque ao extrato
     return saldo, extrato
 
+# Loop principal do programa
 while True:
-    os.system("cls")
+    os.system("cls")  # Limpa a tela do terminal (para Windows)
     print("Informe a operação desejada:")
     print("")
-    print("1 - DEPOSITO")
+    print("1 - DEPÓSITO")
     print("2 - EXTRATO")
     print("3 - SAQUE")
     print("0 - ENCERRAR")
 
     escolha = int(input())
-    os.system("cls")
+    os.system("cls")  # Limpa a tela do terminal (para Windows)
 
-    if escolha == 1:
+    # Realiza a operação escolhida pelo usuário
+    if escolha == 1:  # Depósito
         print("Informe o valor do depósito:")
         valor = float(input())  # Agora a entrada será tratada como float
         if valor <= 0:
@@ -34,14 +44,14 @@ while True:
             continue
         saldo, extrato = deposito(saldo, extrato, valor)
 
-    elif escolha == 2:
+    elif escolha == 2:  # Extrato
         if extrato == "":
             print("Não foram realizadas movimentações.")
             continue
         print(extrato)
         continue
 
-    elif escolha == 3:
+    elif escolha == 3:  # Saque
         if saque_diario < 3:
             print("Informe o valor do saque:")
             valor = float(input())  # Agora a entrada será tratada como float
@@ -55,10 +65,10 @@ while True:
                 print("Saldo insuficiente!")
                 continue
             saldo, extrato = saque(saldo, extrato, valor)
-            saque_diario += 1  # Incrementando o contador de saques diários
+            saque_diario += 1  # Incrementa o contador de saques diários
         else:
             print("Limite de saques em um único dia atingidos.")
             continue
 
-    elif escolha == 0:
+    elif escolha == 0:  # Encerrar
         break
